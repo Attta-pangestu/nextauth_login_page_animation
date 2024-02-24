e2877d0e9437bde195aa9da8bda8b90f5551103c
atha <athabarokahfc@gmail.com>
2024-02-24 23:33:49 +0700

    final animation

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -5,9 +5,9 @@ const Layout = ({ children }) => {
     <main className="flex h-screen bg-blue-400">
       <div className="m-auto w-3/5 h-3/4 grid lg:grid-cols-2 bg-slate-50 ">
         <div className={style.imgWrapper}>
-          {/* <div className={style.cartoonImg}></div> */}
+          <div className={style.cartoonImg}></div>
           <div className={style.cloud_one}></div>
-          <div className={style.clound_two}></div>
+          <div className={style.cloud_two}></div>
         </div>
 
         <div className="right flex flex-col justify-evenly bg-green-400">
